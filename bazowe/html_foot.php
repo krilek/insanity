0054782cbfd10da721ba0770a99a0ddff61d6e6c
krilek <krilek@gmail.com>
2017-07-13 11:02:12 +0200

    Gitignore fix

--- bazowe/html_foot.php
+++
@@ -1,8 +0,0 @@
-<?php
-  echo '<script src="'.JS_KAT.'jquery-3.2.1.min.js"></script>';
-  echo '<script src="'.JS_KAT.'bootstrap.min.js"></script>';
-  if (isset($rejestracja)) {
-      echo '<script src="'.JS_KAT.'rejestracja.js?x='.rand(1, 10).'"></script>';
-  }
-  echo '<script src="'.JS_KAT.'navbar.fix.js?v='.time().'"></script>';
-  echo '<script src="'.JS_KAT.'tooltip.js?v='.time().'"></script>';
